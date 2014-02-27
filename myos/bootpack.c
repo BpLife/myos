9eebdf7fbba80e255e91c2bb9806423fa1d5317b
bplife <52715027@qq.com>
2014-02-27 21:49:00 +0800

    day11，显示窗口

--- myos/bootpack.c
+++ myos/bootpack.c
@@ -19,8 +19,8 @@ void HariMain(void)
 	unsigned int memtotal;
 	
 	struct SHTCTL *shtctl;
-	struct SHEET *sht_back, *sht_mouse;
-	unsigned char *buf_back, buf_mouse[256];
+	struct SHEET *sht_back, *sht_mouse , *sht_win;
+	unsigned char *buf_back, buf_mouse[256],*buf_win;
 	
 	
 	init_gdtidt();
@@ -67,9 +67,20 @@ void HariMain(void)
 	mx = (binfo->scrnx - 16) / 2; /* 中间位置 */
 	my = (binfo->scrny - 28 - 16) / 2;
 	sheet_slideSuper( sht_mouse, mx, my);
+	
+	sht_win   = sheet_alloc(shtctl);
+	buf_win   = (unsigned char *) memman_alloc_4k(memman, 160 * 68);
+	sheet_setbuf(sht_win, buf_win, 160, 68, -1); /* 没有透明色 */
+	make_window8(buf_win, 160, 68, "window");
+	sheet_slideSuper(sht_win, 80, 72);
+
+	
+	
 	/*设置图层高度，并显示*/
 	sheet_updown(sht_back,  0);
-	sheet_updown( sht_mouse, 1);
+	sheet_updown(sht_win,   1);
+	sheet_updown(sht_mouse, 2);
+	
 	
 	
 	
@@ -182,7 +193,54 @@ int mouse_decode(struct MOUSE_DEC* mousedec,unsigned char mousedata)
 	return -1;
 }
 
-
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
+{
+	static char closebtn[14][16] = {
+		"OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+	};
+	int x, y;
+	char c;
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         xsize - 1, 0        );
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         xsize - 2, 1        );
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         0,         ysize - 1);
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         1,         ysize - 2);
+	boxfill8(buf, xsize, COL8_848484, xsize - 2, 1,         xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, xsize - 1, 0,         xsize - 1, ysize - 1);
+	boxfill8(buf, xsize, COL8_C6C6C6, 2,         2,         xsize - 3, ysize - 3);
+	boxfill8(buf, xsize, COL8_000084, 3,         3,         xsize - 4, 20       );
+	boxfill8(buf, xsize, COL8_848484, 1,         ysize - 2, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, 0,         ysize - 1, xsize - 1, ysize - 1);
+	putfonts8_asc(buf, xsize, 24, 4, COL8_FFFFFF, title);
+	for (y = 0; y < 14; y++) {
+		for (x = 0; x < 16; x++) {
+			c = closebtn[y][x];
+			if (c == '@') {
+				c = COL8_000000;
+			} else if (c == '$') {
+				c = COL8_848484;
+			} else if (c == 'Q') {
+				c = COL8_C6C6C6;
+			} else {
+				c = COL8_FFFFFF;
+			}
+			buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+		}
+	}
+	return;
+}
 
 
 /*
