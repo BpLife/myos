fe788fabcd8718e8c97812c564b21e7279c88f3c
bplife <52715027@qq.com>
2014-03-13 20:42:39 +0800

    day_17.对大小写字母的转换，还有键盘指示灯的操作

--- myos/bootpack.c
+++ myos/bootpack.c
@@ -7,7 +7,7 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void task_b_main(struct SHEET *sht_back);
 void console_task(struct SHEET *sheet);
-
+#define KEYCMD_LED		0xed
 
 
 void HariMain(void)
@@ -15,9 +15,7 @@ void HariMain(void)
 
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;//the boot info saved in asmhead
 	
-	struct FIFO32 fifo;
 	char s[40];
-	int fifobuf[128];
 	int mx, my, i, cursor_x, cursor_c;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
@@ -25,20 +23,36 @@ void HariMain(void)
 	//内存管理结构地址
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 	struct SHTCTL *shtctl;
-	static char keytable[0x54] = {
+	static char keytable0[0x80] = {
 		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
 		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
 		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
 		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
 		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
-		'2', '3', '0', '.'
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  0
+	};
+	static char keytable1[0x80] = {
+		0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*', 0,   0,   '}', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
 	};
 	
 	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_win_b, *buf_cons;
 	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_win_b[3], *sht_cons;
 	struct TASK *task_a, *task_b[3], *task_cons;
 	struct TIMER *timer;
-	int key_to = 0;
+	int key_to = 0, key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
+	struct FIFO32 fifo, keycmd;
+	int fifobuf[128], keycmd_buf[32];
+	
+	
 	
 	init_gdtidt();
 	init_pic();//init the programed interrupt controller.
@@ -50,7 +64,7 @@ void HariMain(void)
 	enable_mouse(&fifo, 512, &mdec);//鼠标中断
 	io_out8(PIC0_IMR, 0xf8); /* PIC1  允许 0(timer),1,2号中断，屏蔽其他(11111000) */
 	io_out8(PIC1_IMR, 0xef); /* 允许c号中断，屏蔽其他(11101111) */
-	
+	fifo32_init(&keycmd, 32, keycmd_buf, 0);
 	//内存管理初始化
 	memtotal = memtest(0x00400000, 0xbfffffff);
 	memman_init(memman);
@@ -155,10 +169,19 @@ void HariMain(void)
 	sprintf(s, "memory %dMB   free : %dKB",
 			memtotal / (1024 * 1024), memman_total(memman) / 1024);
 	putfonts8_asc_sht(sht_back, 0, 32, COL8_FFFFFF, COL8_008484, s, 40);
-
-
+	
+	
+	//键盘指示灯
+	fifo32_put(&keycmd, KEYCMD_LED);
+	fifo32_put(&keycmd, key_leds);
 	
 	for (;;) {
+		if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0) {
+				/* 如果存在向键盘控制器发送的的数据，则发送 */
+				keycmd_wait = fifo32_get(&keycmd);
+				wait_KBC_sendready();
+				io_out8(PORT_KEYDAT, keycmd_wait);
+		}
 		io_cli();
 		if (fifo32_status(&fifo) == 0) {
 			task_sleep(task_a);
@@ -170,17 +193,30 @@ void HariMain(void)
 			if (256 <= i && i <= 511) { /*键盘数据 */
 				sprintf(s, "%02X", i - 256);
 				putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-				if (i < 0x54 + 256 && keytable[i - 256] != 0) { /* 一般字符 */
+				if (i < 0x80 + 256) { /* 将按键编码转换成字符编码 */
+					if (key_shift == 0) {
+						s[0] = keytable0[i - 256];
+					} else {
+						s[0] = keytable1[i - 256];
+					}
+				} else {
+					s[0] = 0;
+				}
+				if ('A' <= s[0] && s[0] <= 'Z') {	/* 当输入的字符为英文字母时 */
+					if (((key_leds & 4) == 0 && key_shift == 0) ||
+							((key_leds & 4) != 0 && key_shift != 0)) {
+						s[0] += 0x20;	/* 将大写字母转换时为小写字母 */
+					}
+				}
+				if (s[0] != 0) { /* 一般字符 */
 					if (key_to == 0) {	/* 发送给任务A */
 						if (cursor_x < 128) {
-							/* 显示一个字符后光标移位 */
-							s[0] = keytable[i - 256];
 							s[1] = 0;
 							putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1);
 							cursor_x += 8;
 						}
 					} else {	/* 发送给命令行窗口 */
-						fifo32_put(&task_cons->fifo, keytable[i - 256] + 256);
+						fifo32_put(&task_cons->fifo, s[0] + 256);
 					}
 				}
 				if (i == 256 + 0x0e) {	/* 退格键 */
@@ -193,6 +229,21 @@ void HariMain(void)
 					} else {	/* 发送给命令窗口 */
 						fifo32_put(&task_cons->fifo, 8 + 256);
 					}
+				}
+				if (i == 256 + 0x3a) {	/* CapsLock */
+					key_leds ^= 4;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x45) {	/* NumLock */
+					key_leds ^= 2;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x46) {	/* ScrollLock */
+					key_leds ^= 1;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
 				}
 				if (i == 256 + 0x0f) { /* Tab */
 					if (key_to == 0) {
@@ -208,7 +259,19 @@ void HariMain(void)
 					sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
 				}
 				
-			
+				if (i == 256 + 0x2a) {	/* ON */
+					key_shift |= 1;
+				}
+				
+				if (i == 256 + 0x36) {	/*  ON */
+					key_shift |= 2;
+				}
+				if (i == 256 + 0xaa) {	/*  OFF */
+					key_shift &= ~1;
+				}
+				if (i == 256 + 0xb6) {	/*  OFF */
+					key_shift &= ~2;
+				}
 				boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
 				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);	
 			} else if (512 <= i && i <= 767) { /* 鼠标数据 */
