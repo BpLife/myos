30298ec68e689c1895b5628e5c332619493a1c0c
bplife <52715027@qq.com>
2014-03-17 17:59:26 +0800

    day_20,整理了下代码，更有调理和可读性

--- myos/windows.c
+++ myos/windows.c
@@ -1,26 +1,6 @@
 #include"bootpack.h"
 
-int cons_newline(int cursor_y, struct SHEET *sheet)
-{
-	int x, y;
-	if (cursor_y < 28 + 112) {
-		cursor_y += 16; 
-	} else {
-		
-		for (y = 28; y < 28 + 112; y++) {
-			for (x = 8; x < 8 + 240; x++) {
-				sheet->buf[x + y * sheet->bxsize] = sheet->buf[x + (y + 16) * sheet->bxsize];
-			}
-		}
-		for (y = 28 + 112; y < 28 + 128; y++) {
-			for (x = 8; x < 8 + 240; x++) {
-				sheet->buf[x + y * sheet->bxsize] = COL8_000000;
-			}
-		}
-		sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
-	}
-	return cursor_y;
-}
+
 
 void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
 {
